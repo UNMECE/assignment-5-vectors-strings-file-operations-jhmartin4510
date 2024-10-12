ed63cadaca1450963512347c30a32b11ca56b3f9
Jack Martin <jhmartin4510@triton.unm.edu>
2024-10-12 12:42:31 -0600

    This commit includes comments for functions and for compiling

--- main.cpp
+++ main.cpp
@@ -6,38 +6,44 @@
 #include "pixel_func.h"
 #define SIZE 131072
 
+/*If using UNM linux enviroment, compile using: g++ main.cpp pixel_func.cpp -std=c++11
+ * Run executable with pixels.dat on the command. Will return the flipped.dat file*/
+
 
 int main(int argc, char** argv)
 {
 
-char *file = argv[1];
+char *file = argv[1];				//assigning command line entry to a char* 
 
-std::ofstream MyFile;
-MyFile.open(file,std::ios::out);
-std::string ss = makedata(512,256);
-MyFile<<ss;
-MyFile.close();
+/*Opening a file with the name given by the command line entry and writing in pixel data given the describe x,y dimensions.This data will written in as a string and input in the csv format. */
+std::ofstream MyFile;			
+MyFile.open(file,std::ios::out);		//Opening pixels.dat file for writing into
+std::string ss = makedata(512,256); 
+MyFile<<ss;				
+MyFile.close();					 
 
+/*Opening the pixels.dat file and reading each line of the file to obtain the 5 data types that comprise the Pixel struct(x,y,r,g,b). Each line is entered into the Pixel* array that is then pushed into the Pixel* vector named as pixel_list*/
 std::ifstream InputFile;
 InputFile.open(file,std::ios::in);
-std::vector<Pixel*> pixel_list;
+std::vector<Pixel*> pixel_list;			//list of Pixel*
 std::string line;
-Pixel *p = new Pixel[SIZE];
+Pixel *p = new Pixel[SIZE];			//declaringa Pixel* array whose address will be input into the Pixel* vector
 int ct =0;
 while(!InputFile.eof())
 {
 	std::getline(InputFile, line);
 	if(ct<SIZE)
 	{
-		add_pixel(line, (p+ct), pixel_list);
+		add_pixel(line, (p+ct), pixel_list);  //(p+ct) traverses the Pixel* array so that the next address can be used to input into the pixel_list vector
 	}
 	ct++;	
 }
 InputFile.close();
 
-std::cout<<"Size of list is: " <<pixel_list.size() <<std::endl;
-average_colors(pixel_list);
-flip_vertically(pixel_list);
+/*Running the requested Pixel functions*/
+std::cout<<"Size of list is: " <<pixel_list.size() <<std::endl; //Checking size of pixel_list vector equates to requested size
+average_colors(pixel_list);	
+flip_vertically(pixel_list);			
 
 //Code for creating the flipped.dat file and entering the data for the transformed pixel vector into a string in the csv form
 std::ofstream OutFile;
@@ -46,6 +52,6 @@ std::string os = outputdata(pixel_list);
 OutFile<<os;
 OutFile.close();
 
-delete[] p;
+delete[] p;				//free Pixel* array 
 return 0;
 }
--- pixel_func.cpp
+++ pixel_func.cpp
@@ -6,16 +6,16 @@
 #include <ctime>
 #include "pixel.h"
 #include "pixel_func.h"
-
+/*This function adds a pixel* to the pixel_list vector from the line string given*/
 void add_pixel(std::string line, Pixel *p, std::vector<Pixel *> &pixel_list)
 {
 	int prev_find = 0;
 	int last_find = line.find(",");
 	int ct = 1;
-	while(last_find != std::string::npos)
+	while(last_find != std::string::npos)			//loop for each "," index
 	{
-		std::string sub = line.substr(prev_find, last_find-prev_find);
-		switch(ct)
+		std::string sub = line.substr(prev_find, last_find-prev_find);	//create a substring using indices before and after a comma
+		switch(ct)		//using switch case to convert substring to correct data type. initialzing obtain pointed by p to the converted value of the substring
 		{
 			case 1:
 				p->x = std::stoi(sub);
@@ -34,11 +34,11 @@ void add_pixel(std::string line, Pixel *p, std::vector<Pixel *> &pixel_list)
 		last_find = line.find(",",last_find+1);
 		sub = line.substr(prev_find, line.length()+1-prev_find);
 		p->b = std::stof(sub);
-		ct++;
+		ct++;			//ct increments to do the proper conversion knowing the order of the data being reading by the file
 	}
-	pixel_list.push_back(p);	
+	pixel_list.push_back(p);	//insert p* in pixel_list vector	
 }
-
+/*This function takes in the address of Pixel* vector and traverses the vector to add each value of a particular color. This total color value is then averaged by the size of the pixel_list*/
 void average_colors(std::vector<Pixel*>& pixel_list)
 {	
 	double r =0;
@@ -59,25 +59,25 @@ void average_colors(std::vector<Pixel*>& pixel_list)
 	std::cout<<"Average b is: " <<avg_b <<std::endl;	
 	std::cout<<"----------------" <<std::endl;
 }
-
+/*This function takes in the address of Pixel* vector and flips the pixel data between the first and last y coordinate for a given x coordinate. This does so by finding the index for when x changes value and uses a loops to flip the last and first pixel data for a given x coordinate.*/
 void flip_vertically(std::vector<Pixel*>& pixel_list)
 {
 	Pixel p;
 	int start = 0;
 	int half = 0;
-	int next = pixel_list[0]->x;
+	int next = pixel_list[0]->x;		//initializing value to starting x value
 	int max = 0;
-	for(int i = 0; i<pixel_list.size(); i++)
+	for(int i = 0; i<pixel_list.size(); i++)	//loop through entire vector list
 	{
-		if(pixel_list[i]->x != next)
+		if(pixel_list[i]->x != next)	//find when x value changes
 		{
-			max = i-1;
-			if((i-start) % 2 != 0)
+			max = i-1;		//i-1 is the previous index from the changed x
+			if((i-start) % 2 != 0)	//find halfway point and considering even and odd number of switches
 			{
 				half = ((max-start)/2)+start;	
 			}
 			half = ((i-start)/2)+start;
-			for(int j = 0;start<half;start++, j++)
+			for(int j = 0;start<half;start++, j++)		//looping from start index to the halway point and flipping the first and last pixel objects for a given x incrementally
 			{
 				p = *pixel_list[start];
 				pixel_list[start]->r = pixel_list[max-j]->r;
@@ -87,9 +87,9 @@ void flip_vertically(std::vector<Pixel*>& pixel_list)
 				pixel_list[max-j]->g = p.g;
 				pixel_list[max-j]->b = p.b;
 			}
-			start = i;
+			start = i;		//initialize start to i, where the change was observed
 		}
-		else if(i==(pixel_list.size()-1))
+		else if(i==(pixel_list.size()-1))	//considering the list of the last x value
 		{
 			max = pixel_list.size();
 			if((max-start)%2!=0)
@@ -108,16 +108,16 @@ void flip_vertically(std::vector<Pixel*>& pixel_list)
 				pixel_list[max-j]->b = p.b;
 			}
 		}
-		next = pixel_list[i]->x;
+		next = pixel_list[i]->x;		//initializing next to the value of x from the ith object in the pixel_list each time the loop runs
 	}	
 }
-
+/*This function makes "random" pixel data for the given xy dimensions.This pixel data is converted into a long string and returns the string*/
 std::string makedata(int x, int y)
 {
-	Pixel p1;
-	std::ostringstream ss;
+	Pixel p1;			//declaring a pixel object
+	std::ostringstream ss;		//using ostringstream object to appended pixel data
 	
-	srand((unsigned) time(NULL));
+	srand((unsigned) time(NULL));	//setting random number generator
 	for(int i = 0; i<x; i++)
 	{
 		for(int j = 0; j<y; j++)
@@ -130,9 +130,9 @@ std::string makedata(int x, int y)
 			ss <<p1.x <<"," <<p1.y <<"," <<p1.r <<"," <<p1.g <<"," <<p1.b <<std::endl;
 		}
 	}
-	return ss.str();
+	return ss.str();		//return a string object from the appended stringstream object
 }
-
+/*This function references the pixel_list vector obejects and converts each object of the vector into a long appended string*/
 std::string outputdata(std::vector<Pixel*>& pixel_list)
 {
 	std::ostringstream os;
